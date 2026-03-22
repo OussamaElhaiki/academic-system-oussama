e03ad23d2218b1d42639da383469ab60376c3705
OussamaElhaiki <oussama.elhaiki@gmail.com>
2026-03-22 19:16:14 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Image from "next/image"
 
 export default function Home() {
+
+  console.log("Hello from client")
+  //console.log(x)
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+        git init<Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -14,7 +18,10 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Oussama First Next App
+          </h1>
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            {process.env.NEXT_PUBLIC_APP_NAME}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -61,5 +68,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  );
+  )
 }
